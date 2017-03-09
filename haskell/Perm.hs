b0d20be142b193a95766d4bfc1961544e57fc89a
Kai Dainichi Christensen <dainichi@gmail.com>
2017-03-09 23:37:42 +0900

    Substantial performance improvement in permutation algorithm by not checking list equality for non-cyclic permutations (as in the Python implementation)

--- haskell/Perm.hs
+++ haskell/Perm.hs
@@ -18,32 +18,30 @@ instance NubDel DupList where
     nubDel (DupList ((x,n):l)) | n > 0 = (x,DupList ((x,n-1):l)): map (\(y,DupList m) -> (y, DupList ((x,n):m))) (nubDel (DupList l))
                                | n == 0 = nubDel (DupList l)
 
-lc xs ys = 
+cyclic xs ys = 
     case (popFront xs, popFront ys) of
-        (Just (x,xx), Just (y,yy)) -> 
-            case compare x y of
-                EQ -> lc xx yy
-                ord -> ord
-        _ -> EQ
+        (Just (x,xx), Just (y,yy)) -> x==y && cyclic xx yy
+        _ -> True
 
 --a permutation algorithm which only generates the lexicographically smallest rotation, i.e. a cononical form representing the cyclic permutation. It makes sure that no subsequence is lexicographically smaller than the whole sequence generated so far. nym (not yet matched) is the remainder of the currently generated sequence when removing the longest subsequence currently generated which equals a prefix of the currently generated sequence. I have not rigidly proved correctness.
-perm acc nym l = 
+perm acc nym aeqn l = 
     let nubDels = nubDel l
     in case nubDels of
-        [] -> case lc acc nym of
-                LT -> []
-                EQ -> [acc]
---              _ -> error $ show acc ++ " " ++ show nym
+        [] -> if aeqn || cyclic acc nym 
+              then [acc]
+              else []
         _ -> do
                 (x,xx) <- nubDels
                 case popFront nym of
                     Just (n,nn) ->
                         case compare x n of
                             LT -> []
-                            EQ -> perm (pushBack acc x) (pushBack nn x) xx
-                            GT -> let pb = pushBack acc x in perm pb pb xx
-                    _ -> let pb = pushBack acc x in perm pb pb xx
+                            EQ -> perm (pushBack acc x) (pushBack nn x) False xx
+                            GT -> let pb = pushBack acc x in perm pb pb True xx
 
-permutation l = perm empty empty l
-
+--permutation l = perm empty empty l
+permutation l = do
+                    (x,xx) <- nubDel l
+                    let pex = pushBack empty x
+                    perm pex pex True xx
 
